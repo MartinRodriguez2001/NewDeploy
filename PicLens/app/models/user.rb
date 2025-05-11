7877680af927a0b2b13619022a0f311878aa865b
bastianc00 <bcepeda@miuandes.cl>
2025-05-11 12:58:29 -0400

    Fix model user

--- PicLens/app/models/user.rb
+++ PicLens/app/models/user.rb
@@ -28,5 +28,5 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }
   
-  has_secure_password
+  #has_secure_password
 end
